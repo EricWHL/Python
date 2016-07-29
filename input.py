704a234602d99a29511c7bb66f87474d7ac6ba32
wanghailong <wanghailong@pset.suntec.net>
2016-07-29 15:10:32 +0800

    add file

---
+++ input.py
@@ -0,0 +1,12 @@
+
+
+name = raw_input('Your name is:')
+age = raw_input('Your age is:')
+
+print '**************************************'
+print '*  Name is %s                        *' % name
+print '*  Age  is %d                        *' % age
+print '*                                    *'
+print '*                                    *'
+print '*                                    *'
+print '**************************************'
