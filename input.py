cd5d6f481a021acd7b742ccaf862e4fa9d014587
WangHailong <396568798@qq.com>
2016-07-30 21:53:06 +0800

    modify for file

--- input.py
+++ input.py
@@ -1,12 +1,14 @@
 
+###this program is for input string
 
 name = raw_input('Your name is:')
 age = raw_input('Your age is:')
+company = raw_input('Your company is:')
 
 print '**************************************'
-print '*  Name is %s                        *' % name
-print '*  Age  is %d                        *' % age
-print '*                                    *'
-print '*                                    *'
-print '*                                    *'
+print 'Name is %s ' % name
+print 'Age  is %s ' % age
+print 'Company is %s ' % company
+print ''
+print ''
 print '**************************************'
